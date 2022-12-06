bba3e3610b5d08706c9056a4f9067a984c59926d
Janice Dasoy <jandash01@gmail.com>
2022-12-06 18:52:45 +0800

    Janice

---
+++ IDRegistrationCrud-main/index.php
@@ -0,0 +1,13 @@
+<?php
+include 'functions.php';
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p>Welcome!</p>
+		<a href="create.php" class="create-contact">New Students is enrolled!</a>
+</div>
+
+<?=template_footer()?>
